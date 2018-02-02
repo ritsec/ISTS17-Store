311bf337aa036e0cdf72b4c2be0666be675d9aa3
Sean Sun <sean@seanqsun.com>
2018-02-02 17:10:31 -0500

    frontend done yay

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,20 +2,3 @@ $(document).ready(function(){
     $("#login-modal").modal('show');
 });
 
-$(function() {
-    $('button').click(function(e) {
-        console.log(e.target.id);
-        $.post(
-            '/shop', 
-            {
-                item_id: e.target.id
-            },
-            function(data, status){
-                $("#result").html(data);
-            }
-        )
-    });
-});
-
-
-   
