3e9f38f3705d04d11dc63edc0249c5e2e2e67b0f
Doshmajhan <cpc1007@g.rit.edu>
2018-02-03 04:24:58 -0500

    using ips

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@
     Config parameters for our web app
 """
 
-BANK_API_URL = "http://atlas.whiteteam.ritsec:5000"
-AUTH_API_URL = "http://atlas.whiteteam.ritsec:9000"
+BANK_API_URL = "http://10.0.20.21:5000"
+AUTH_API_URL = "http://10.0.20.21:9000"
 SECRET_KEY = 'thenotsosecretkey'
 
 AUTH_ENDPOINTS = ['validate-session', 'login', 'update-password',
