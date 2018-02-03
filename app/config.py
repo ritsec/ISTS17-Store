2b3f5b0c6ab60f955009756549c9aa654519ed40
Doshmajhan <cpc1007@g.rit.edu>
2018-02-03 01:06:20 -0500

    updating api urls

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@
     Config parameters for our web app
 """
 
-BANK_API_URL = "http://lilbite.org:5000"
-AUTH_API_URL = "http://lilbite.org:9000"
+BANK_API_URL = "http://atlas.whiteteam.ritsec:5000"
+AUTH_API_URL = "http://atlas.whiteteam.ritsec:9000"
 SECRET_KEY = 'thenotsosecretkey'
 
 AUTH_ENDPOINTS = ['validate-session', 'login', 'update-password',
